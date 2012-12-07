edb4d3b75c539cc7da56c97d6f1ab2756cb5c3bc
Chris Alfonso <calfonso@redhat.com>
2012-12-07 11:54:19 -0500

    BZ877158 -  No "log out" button exists for the web console when using basic auth

---
+++ openshift-console/app/controllers/console_index_controller.rb
@@ -0,0 +1,16 @@
+class ConsoleIndexController < ConsoleController
+  skip_before_filter :authenticate_user!, :only => [:unauthorized, :logout]
+
+  def index
+    redirect_to applications_path
+  end
+  def unauthorized
+    render 'console/unauthorized'
+  end
+  def logout
+    redirect_to "https://.@#{request.host}/console"
+  end
+  def help
+    render 'console/help'
+  end
+end
--- openshift-console/config/routes.rb
+++ openshift-console/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   openshift_console
+  match 'logout' => 'console_index#logout', :via => :get, :as => 'logout'
   root :to => 'console_index#index'
 end
