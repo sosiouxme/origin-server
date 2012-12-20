67347677ea967c6d593e9f259c8f580b70933959
Brenton Leanhardt <bleanhar@redhat.com>
2012-12-20 15:26:05 -0500

    Tweaking the regex in restorer.php and escaping the arg

--- node-util/www/html/restorer.php
+++ node-util/www/html/restorer.php
@@ -1,10 +1,17 @@
 <?php
 
 list($blank, $uuid, $blank) = split("/", $_SERVER["PATH_INFO"]);
-shell_exec("/usr/sbin/oo-restorer-wrapper.sh $uuid");
-
-sleep(2);
-$url=str_replace("/$uuid", "", $_SERVER["PATH_INFO"]);
-header("Location: $url");
-
+if (preg_match('/^[0-9a-fA-F]{32}$/', $uuid)) {
+    $safe_uuid = escapeshellarg($uuid);
+    shell_exec("/usr/sbin/oo-restorer-wrapper.sh $safe_uuid}");
+    sleep(2);
+    $host = $_SERVER['HTTP_HOST'];
+    $proto = "http" . ( isset($_SERVER['HTTPS']) ? 's' : '' ) . '://';
+    $url=str_replace("/$uuid", "", $_SERVER["PATH_INFO"]);
+    header("Location: $proto$host$url");
+} else {
+    // someone is trying to attack
+    error_log("Invalid uuid $uuid given to restorer.php");
+    header('HTTP/1.0 403 Forbidden');
+}
 ?>
